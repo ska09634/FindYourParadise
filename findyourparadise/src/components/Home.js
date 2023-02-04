856fc9810b4656e57020f115c21e4a0b423a06cc
ska09634 <ska09634@uga.edu>
2023-02-04 15:44:00 -0500

    basic html for home page

--- findyourparadise/src/components/Home.js
+++ findyourparadise/src/components/Home.js
@@ -1,6 +1,13 @@
 function Home() {
     return (
-        <h1>Home page for find your paradise</h1>
+        <div>
+            <div className="header"><h1>FIND YOUR</h1></div>
+            <div className="paradise"><h1>Paradise</h1></div>
+            <div className="description"><p>TAKE A SHORT QUIZ AND FIND YOUR HOME AWAY FROM HOME.</p></div>
+            <div className="button">
+                <button>TAKE THE QUIZ</button>
+            </div>
+        </div>
     );
 }
 
