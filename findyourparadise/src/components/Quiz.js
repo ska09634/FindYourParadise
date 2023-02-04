e8ef7896afe1874d0d6230286c53136054775ec8
ska09634 <ska09634@uga.edu>
2023-02-04 18:00:35 -0500

    save

--- findyourparadise/src/components/Quiz.js
+++ findyourparadise/src/components/Quiz.js
@@ -3,15 +3,9 @@ import './Quiz.css';
 
 function Quiz() {
     return (
-<<<<<<< HEAD
+        <>
         <div>
         <h1>QUIZ</h1>
-=======
-        <>
-        <div id='Quiz'>
-        <h1>Quiz page for find your paradise</h1>
-        <p>Question 1</p>
->>>>>>> 0e682669ac39666583466b76eb9a47def2a2c543
         </div>
         </>
     );
