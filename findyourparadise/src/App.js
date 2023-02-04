1276c3cb3b23e3904d57d286d87fe7a9f0dbd1f4
ska09634 <ska09634@uga.edu>
2023-02-04 15:24:50 -0500

    remade Home component

--- findyourparadise/src/App.js
+++ findyourparadise/src/App.js
@@ -1,28 +1,12 @@
 import './App.css';
 import Home from './components/Home';
 import Quiz from './components/Quiz';
-
+import Results from './components/Results';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello from Jayla again</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Home />
   );
 }
 
