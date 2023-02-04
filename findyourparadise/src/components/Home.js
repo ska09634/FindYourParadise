0b883a3a208530342d75c694fd861d9d03581de5
ska09634 <ska09634@uga.edu>
2023-02-04 15:17:16 -0500

    added components to App

---
+++ findyourparadise/src/components/Home.js
@@ -0,0 +1,7 @@
+function Home() {
+    return (
+        <h1>Home page for find your paradise</h1>
+    );
+}
+
+export default Home;
---
+++ findyourparadise/src/components/Results.js
@@ -0,0 +1,7 @@
+function Results() {
+    return (
+        <h1>Results page for find your paradise</h1>
+    );
+}
+
+export default Results;
