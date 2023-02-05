ab6d7f865ca44388b211036f3aebe5d5c7dd215f
ska09634 <ska09634@uga.edu>
2023-02-04 20:24:29 -0500

    home css

--- findyourparadise/src/components/Home.js
+++ findyourparadise/src/components/Home.js
@@ -1,5 +1,5 @@
-import { Link, useNavigate } from 'react-router-dom'
-import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
+import './Home.css';
 
 function Home() {
     let navigate = useNavigate();
@@ -11,6 +11,7 @@ function Home() {
         <div>
             <div className="header"><h1>FIND YOUR</h1></div>
             <div className="paradise"><h1>Paradise</h1></div>
+            <div class="img_header"><img src="images/PalmTrees.png"  alt="palm trees"></img></div>
             <div className="description"><p>TAKE A SHORT QUIZ AND FIND YOUR HOME AWAY FROM HOME.</p></div>
             <div className="button">
                 <button onClick={handleClick}>Quiz</button>
