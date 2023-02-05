a6913410e63530354429f7218c36c174acf22096
Alona0420 <you@example.com>
2023-02-04 21:25:44 -0500

    more quiz css stages

--- findyourparadise/src/components/Quiz.js
+++ findyourparadise/src/components/Quiz.js
@@ -1,5 +1,9 @@
 import React from "react";
 import './Quiz.css';
+import leftIMG from '../images/QuizLeft.png';
+import rightIMG from '../images/QuizRight.png'
+
+console.log(leftIMG);
 
 function Quiz() {
     return (
@@ -9,7 +13,9 @@ function Quiz() {
  
        
         <body className="background">
-        <h1>Quiz </h1>
+            <img className="picLeft" src={leftIMG} alt="travel icon"/>
+            <h1>Quiz </h1>
+            <img className="picRight" src={rightIMG} alt="house icon"/>
         <div className="rec">
         <p>Question 1 </p>
         <br></br>
