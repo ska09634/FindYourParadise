4f2b75db233c7907bc8844838133ed8224e33f04
MDCodesz <michaeladano.cs@gmail.com>
2023-02-04 21:39:45 -0500

    from1

--- findyourparadise/src/components/Quiz.js
+++ findyourparadise/src/components/Quiz.js
@@ -3,26 +3,28 @@ import './Quiz.css';
 
 function Quiz() {
     return (
-        <>
         <div className= "container-horizontal">
- 
- 
-       
         <body className="background">
-        <h1>Quiz </h1>
-        <div className="rec">
-        <p>Question 1 </p>
-        <br></br>
-        <p>Question 2</p>
-        </div>
-        </body>
-<<<<<<< HEAD
+
+            <form>
+                
+                <label>Question 1: What speaks to you more? Nature, Beaches, Amusement Parks, Museusm, or Sports?</label>
+                <select>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <select>
+                    <option value="nature">Nature</option>
+                    <option value="beaches">Beaches</option>
+                    <option value="aparks">Amusement Parks</option>
+                    <option value="museums">Museums</option>
+                    <option value="sports">Sports</option>
+                </select>
+            </form>
         
-=======
-       
->>>>>>> a973f240fbfe587634d5f53705e75ff9542e6c21
-        </div>
-        </>
+        </body>
+    
+    </div>
     );
  }
  
