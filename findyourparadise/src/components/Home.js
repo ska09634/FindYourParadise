286657bf7eff77c92c2c3e13b5b1309f66953c25
ska09634 <ska09634@uga.edu>
2023-02-04 22:35:33 -0500

    home page done!!!!!!

--- findyourparadise/src/components/Home.js
+++ findyourparadise/src/components/Home.js
@@ -1,5 +1,8 @@
 import { useNavigate } from 'react-router-dom'
 import './Home.css';
+import plane from '../images/plane border.jpg'
+
+console.log(plane);
 
 function Home() {
     let navigate = useNavigate();
@@ -17,6 +20,8 @@ function Home() {
             <div className="button">
                 <button onClick={handleClick}>TAKE THE QUIZ</button>
             </div> 
+            <img className="left" src={plane} alt="plane" width="400px"/>
+            <img className="right" src={plane} alt="plane" width="400px"/>
          </div>       
     );
 }
