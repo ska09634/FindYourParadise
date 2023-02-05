2087e674da0aca4e74c465768ad776625aff497b
ska09634 <ska09634@uga.edu>
2023-02-04 20:44:20 -0500

    home page layout

--- findyourparadise/src/components/Home.js
+++ findyourparadise/src/components/Home.js
@@ -9,9 +9,10 @@ function Home() {
 
     return (
         <div>
-            <div className="header"><h1>FIND YOUR</h1></div>
-            <div className="paradise"><h1>Paradise</h1></div>
-            <div class="img_header"><img src="images/PalmTrees.png"  alt="palm trees"></img></div>
+            <div className="banner">
+                <div className="header"><h1>FIND YOUR</h1></div>
+                <div className="paradise"><h1>Paradise</h1></div>
+            </div>
             <div className="description"><p>TAKE A SHORT QUIZ AND FIND YOUR HOME AWAY FROM HOME.</p></div>
             <div className="button">
                 <button onClick={handleClick}>Quiz</button>
